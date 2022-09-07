f06bffff1d2003c7f644dcd08df95f4f1d26b01d
Francisco Carlos <fcarlosmonteiro@gmail.com>
2022-09-06 21:24:19 -0300

    update map_manipulation

--- algorithm/map_manipulation.py
+++ algorithm/map_manipulation.py
@@ -3,32 +3,29 @@ import folium
 import pandas as pd
 
 class Create_Maps:
+
    def __init__(self):
       center=[-25.753834153585533, -53.05983441738876]
       self.m = folium.Map(location=center, tiles="OpenStreetMap", zoom_start=16)
 
-   def mark_points(self,lat,long):
 
-      data = pd.DataFrame({
+   def plot_route(self,lat,long):
+
+      self.data = pd.DataFrame({
          'lon':long,
          'lat':lat,
          'name':['Bob, Cachorro', 'Tobi','Pipoca','Reginaldo','Totó','Tom','Devil','Jorginho','Gil','Magrelo']
       }, dtype=str)
 
-      # add marker one by one on the map
-      for i in range(0,len(data)):
-         folium.Marker(
-            location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-            popup=data.iloc[i]['name']
-         ).add_to(self.m)
-
-      # Show the map
-      self.m.save('index.html')
-
-   def plot_route(self,lat,long):
-
       loc = list(zip(lat,long))
       print(loc)
+      for i,pet in enumerate(loc):
+         folium.Marker(
+            location=pet,
+            icon=folium.DivIcon(html=f"""<div style="font-family: courier new; font-size:20px; color: blue">{ i }</div>"""),
+            popup=self.data.iloc[i]['name']
+         ).add_to(self.m)
+         
       folium.PolyLine(loc,
                      color='red',
                      weight=10,
